2ce14da4ddf0588463e481eb7aae3d3628444f34
潘子龙 <panzilong2006@126.com>
2020-10-21 17:16:31 +0800

    1hour hot area arrive polygon

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -14,6 +14,25 @@ import html2canvas from 'html2canvas';
 import { circle } from '@turf/turf';
 import { dotData } from '@/mocks/rt_city_loc';
 import { getRegionListD } from '@/mocks/getRegionList';
+import { shoudujichangD } from '@/mocks/shoudujichang';
+import { beijingxizhanD } from '@/mocks/beijingxizhan';
+import { beijingnanzhanD } from '@/mocks/beijingnanzhan';
+import { dongzhimenD } from '@/mocks/dongzhimen';
+import { libafangD } from '@/mocks/libafang';
+import { qingheD } from '@/mocks/qinghe';
+import { sihuiD } from '@/mocks/sihui';
+import { songjiazhuangD } from '@/mocks/songjiazhuang';
+import { tiantongyuanbeiD } from '@/mocks/tiantongyuanbei';
+import { xiyuanD } from '@/mocks/xiyuan';
+import { xizhimenD } from '@/mocks/xizhimen';
+import { yinglanguojiD } from '@/mocks/yinglanguoji';
+import { hademenD } from '@/mocks/hademen';
+import { wangfuD } from '@/mocks/wangfu';
+import { andingmenD } from '@/mocks/andingmen';
+import { shilipuD } from '@/mocks/shilipu';
+import { taiyanggongD } from '@/mocks/taiyanggong';
+
+const spotMarker = require('assets/red_marker.png');
 
 export default {
   data() {
@@ -28,11 +47,12 @@ export default {
   mounted() {
     this.$nextTick(() => {
       this.initMap();
-      this.initArc();
-      this.initPolygon();
+      // this.initArc();
+      // this.initPolygon();
       // this.tests();
       // this.turf();
       // this.initDot();
+      this.initPolygonMocks();
     });
   },
   methods: {
@@ -41,10 +61,103 @@ export default {
       this.map = new TMap.Map('map', {
         center: new TMap.LatLng(39.916527, 116.397128),
         pitch: 0,
-        mapStyleId: 'style2',
+        mapStyleId: 'style1',
         zoom: 12
       });
       // this.initTextBox();
+    },
+    initPolygonMocks() {
+      const arrPolygon = shoudujichangD.split(/[\n]/);
+      const firstRow = arrPolygon[0];
+      const firstRowArr = firstRow.split(/[\s]/);
+      this.name = firstRowArr[0];
+      const centerLoc = firstRowArr[1];
+      const arrCenterLoc = centerLoc.split(',');
+      this.centerLocation = new TMap.LatLng(arrCenterLoc[1], arrCenterLoc[0]);
+      const geometries = [];
+      arrPolygon.forEach((item, idx) => {
+        const row = item.split(/[\s]/);
+        const ne = row[2];
+        const neArr = ne.split(',');
+        const nePath = new TMap.LatLng(neArr[1], neArr[0]);
+        const sw = row[3];
+        const swArr = sw.split(',');
+        const swPath = new TMap.LatLng(swArr[1], swArr[0]);
+        const nwPath = new TMap.LatLng(neArr[1], swArr[0]);
+        const wePath = new TMap.LatLng(swArr[1], neArr[0]);
+        const path = [nePath, wePath, swPath, nwPath];
+        const obj = {
+          id: `p_${idx}`,
+          styleId: 'polygon',
+          paths: path
+        };
+        geometries.push(obj);
+      });
+      //初始化polygon
+      var polygon = new TMap.MultiPolygon({
+          id: 'polygon-layer', //图层id
+          map: this.map, //显示多边形图层的底图
+          styles: { //多边形的相关样式
+              'polygon': new TMap.PolygonStyle({
+                  'color': '#3777FF', //面填充色
+                  'showBorder':true, //是否显示拔起面的边线
+                  'borderColor': '#00FFFF', //边线颜色
+                  'borderWidth': 1
+              })
+          },
+          geometries
+        });
+        this.initName();
+        this.initMarker();
+    },
+    initMarker() {
+      const marker = [{
+        id: 'marker',
+        styleId: 'marker',
+        position: this.centerLocation,
+        properties: {
+          title: 'marker'
+        }
+      }];
+      this.multiMarkerLayer = new TMap.MultiMarker({
+        id: 'marker-layer-stop',
+        map: this.map,
+        styles: {
+          marker: new TMap.MarkerStyle({
+            width: 39,
+            height: 49,
+            anchor: { x: 18, y: 60 },
+            src: spotMarker
+          })
+        },
+        geometries: marker
+      });
+    },
+    initName() {
+      //初始化label
+        var label = new TMap.MultiLabel({
+            id: 'label-layer',
+            map: this.map,
+            styles: {
+                'label': new TMap.LabelStyle({
+                    'color': '#000', //颜色属性
+                    'size': 20, //文字大小属性
+                    'offset': { x: 0, y: 0 }, //文字偏移属性单位为像素
+                    'angle': 0, //文字旋转属性
+                    'alignment': 'center', //文字水平对齐属性
+                    'verticalAlignment': 'middle' //文字垂直对齐属性
+                })
+            },
+            geometries: [{
+                'id': 'label', //点图形数据的标志信息
+                'styleId': 'label', //样式id
+                'position': this.centerLocation, //标注点位置
+                'content': this.name, //标注文本
+                'properties': { //标注点的属性数据
+                    'title': 'label'
+                }
+            }]
+        });
     },
     initPolygon() {
       console.log(getRegionListD, '<----getRegionListD');
